8a71a8e4065b7ca8f1e72a53d706fe535a4afadc
gwagwaflamingo <namityroblox@gmail.com>
2023-04-04 18:54:15 -0300

    Context for removed scripts

--- Npc/11000096.py
+++ Npc/11000096.py
@@ -5,6 +5,7 @@ import random
 
 class Main(Script):
     def first(self) -> int:
+        # Script ID 50 is a duplicate of 40, labeled feature Soulbinder. Script ID 30 is a pre-restart quest sensitive script, though it is unclear which specific quest that may be.
         return 40
 
     def select(self) -> int:
